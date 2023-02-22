e7aa7daad310634c403ed8ee9f9d14f0ea5cbd46
jasonsaini <jasonsaini.exe@gmail.com>
2023-02-21 21:24:19 -0500

    Added transaction class

--- src/project2/Transaction.java
+++ src/project2/Transaction.java
@@ -1,5 +1,53 @@
+/*
+ Name:
+ Course: CNT 4714 Spring 2023
+ Assignment title: Project 2 -- Synchronized, Cooperating threads Under Locking
+ Due Date: February 12, 2023
+ */
+
 package project2;
 
-public class Transaction {
+import java.util.Random;
 
+public class Transaction {
+	final static int DEPOSIT_RANGE = 500;
+	final static int WITHDRAW_RANGE = 99;
+	final static int AUDIT_RANGE = 10000;
+	
+	public static int generateRandNum(char transaction)
+	{
+		
+		int randomNum = 0;
+		Random random = new Random();
+		if(transaction == 'w')
+		{
+			randomNum = random.ints(1,WITHDRAW_RANGE).findFirst().getAsInt();
+		}
+		else if(transaction == 'd')
+		{
+			randomNum = random.ints(1,DEPOSIT_RANGE).findFirst().getAsInt();
+		}
+		else if(transaction == 'a')
+		{
+			randomNum = random.ints(300, 10000).findFirst().getAsInt();
+		}
+		return randomNum;
+	}
+	
+	public static int randSleepDuration(char transaction)
+	{
+		Random random = new Random();
+		int sleepDur = 0;
+		
+		if(transaction == 'w')
+		{
+			sleepDur = random.nextInt(200);
+		}
+		else if(transaction == 'd')
+		{
+			sleepDur = random.nextInt(500);
+		}
+		
+		return sleepDur;
+	}
 }
