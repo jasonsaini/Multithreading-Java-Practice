bc19a08fbd31ad75bef7a3bf047d1cb1cc2775b6
jasonsaini <jasonsaini.exe@gmail.com>
2023-02-21 21:23:17 -0500

    Added depositor class

--- src/project2/Depositor.java
+++ src/project2/Depositor.java
@@ -1,5 +1,37 @@
+/*
+ Name:
+ Course: CNT 4714 Spring 2023
+ Assignment title: Project 2 -- Synchronized, Cooperating threads Under Locking
+ Due Date: February 12, 2023
+ */
+
 package project2;
 
-public class Depositor {
-
+public class Depositor implements Runnable {
+	final static int DEPOSIT_SLEEP_TIME = 7500;
+	
+	private BankAccount bankAcct;
+	
+	public Depositor(BankAccount sharedAcct)
+	{
+		bankAcct = sharedAcct;
+	}
+	
+	public void run()
+	{
+		//System.out.println("Starting depositor thread");
+		try
+		{
+			while(true)
+			{
+				//System.out.println("Entering infinite depositor loop!");
+				bankAcct.deposit();
+				Thread.sleep(DEPOSIT_SLEEP_TIME);
+			}
+		}
+		catch(InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+	}
 }
